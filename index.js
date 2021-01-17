8b76cff44e702e256dc8d461887624511dd332af
bask-js <back@yonsei.ac.kr>
2021-01-17 22:24:42 +0900

    nodejs

--- index.js
+++ index.js
@@ -1,9 +1,33 @@
 const express = require('express')
 const app = express()
 const port = 5000
+const bodyParser = require('body-parser');
+const { User } = require('./models/User');
+
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.use(bodyParser.json());
+
+const mongoose = require('mongoose')
+mongoose.connect('mongodb+srv://backjs:1234@boiler-plate.rzngb.mongodb.net/<dbname>?retryWrites=true&w=majority',{
+  useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
+}).then(()=>console.log('MONGODB Connected'))
+.catch(err =>console.log(err))
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
+})
+
+app.post('/register',(req,res) => {
+
+  const user = new User(req.body)
+
+  user.save((err,userInfo)=>{
+    if(err) return res.json({success:false,err})
+    return res.status(200).json({
+      success: true
+    })
+  })
 })
 
 app.listen(port, () => {
